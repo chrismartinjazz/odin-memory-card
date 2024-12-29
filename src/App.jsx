477e1a142a8e49bd24ebb121ba2a7b51c930bf7f
Chris Martin <chrismartinjazz@gmail.com>
2024-12-30 08:03:38 +1030

    Added click handling, counter, maxScore to App. Moved getTitle methods from Card to App, and change so title property is added to the photo objects on initial retrieval from API. Add attributions section with link to photo and photographer. Change App to retrieve a longer list of photos and randomly choose a sub-set of of photos on load.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,62 @@
 import "./App.css"
 import { useState, useEffect } from 'react';
 import { getData } from "./getData.js"
-import { getLocalStorage, setLocalStorage } from "./localstorage.js";
+import { getLocalStorage, setLocalStorage, removeLocalStorage } from "./localstorage.js";
 import Card from './components/Card.jsx';
+
+const refreshLocalStorage = false;
+if (refreshLocalStorage) removeLocalStorage();
+
+const photoPoolSize = 12;
+const photosToDisplay = 4;
 
 export default function App() {
   const [photos, setPhotos] = useState([]);
-  // const [clickedIds, setClickedIds] = useState([]);
+  const [clickedIds, setClickedIds] = useState(new Set());
+  const [maxScore, setMaxScore] = useState(0);
 
   useEffect(() => {
-    requestData("curated?page=1&per_page=12")
+    requestData(`curated?page=1&per_page=${photoPoolSize}`)
       .then((result) => {
-        setPhotos(result.photos);
+        setPhotos(addTitleToPhotos(randomSelection(result.photos, photosToDisplay)));
       })
   }, []);
+
+  if (clickedIds.size > maxScore) { setMaxScore(clickedIds.size) };
+
+  if (clickedIds.size >= photosToDisplay) {
+    setClickedIds(new Set());
+  }
+
+  const handleClick = (id) => {
+    if (!clickedIds.has(id)) {
+      const nextClickedIds = new Set([...clickedIds]);
+      nextClickedIds.add(id);
+      setClickedIds(nextClickedIds);
+    } else {
+      setClickedIds(new Set());
+    }
+  }
 
   const photosInRandomOrder = randomizeArray(photos);
   
   return (
     <>
-      <div className="gallery" onClick={(e) => console.log(e.target.id)} >
+      <div className="header">
+        <div className="header__score">Score: {clickedIds.size}</div>
+        <div className="header__maxScore">Max Score: {maxScore}</div>
+      </div>
+      <div className="gallery">
         {photosInRandomOrder.map((photo) => (
-          <Card key={photo.id} photo={photo} />
+          <Card key={photo.id} photo={photo} onClick={handleClick} />
+        ))}
+      </div>
+      <div className="attributions">
+        <a href="https://www.pexels.com">Photos provided by Pexels</a>
+        {photos.map((photo) => (
+          <div key={photo.id}>
+            <a href={photo.url}>{photo.title}</a> by <a href={photo.photographer_url}>{photo.photographer}</a>
+          </div>
         ))}
       </div>
     </>
@@ -34,9 +69,9 @@ query the API and store the data. If there is, retrieve the stored data.
 */
 async function requestData(query) {
   const currentDate = (new Date()).toDateString();
-  const localData = getLocalStorage();
+  const localData = getLocalStorage() || {};
 
-  if (localData[currentDate][query]) {
+  if (localData && localData[currentDate] && localData[currentDate][query]) {
     console.log("Local data retrieved", localData[currentDate][query]);
     return localData[currentDate][query];
   }
@@ -64,3 +99,97 @@ function randomizeArray(input) {
   
   return output;
 }
+
+function randomSelection(array, numberToSelect) {
+  if (!(array instanceof Array) || !Number.isInteger(numberToSelect)) {
+    return new Error("Unexpected arguments to randomSelection()")
+  }
+  if (array.size >= numberToSelect) return array;
+
+  const randomIndices = [];
+  while (randomIndices.length < numberToSelect) {
+    const n = Math.floor(Math.random() * 10);
+    if (!randomIndices.includes(n)) randomIndices.push(n)
+  }
+  randomIndices.sort();
+
+  const output = [];
+  randomIndices.forEach((i) => output.push(array[i]));
+  return output;
+}
+
+function addTitleToPhotos(photos) {
+  photos.forEach(photo => {
+    const title = getTitle(photo.url);
+    photo.title = title;
+  })
+  return photos;
+}
+
+function getTitle(str) {
+  const maxWordCount = 3;
+
+  if (
+    (!str)
+    || (typeof(str) !== "string")
+    || (str.indexOf("photo/") == -1)
+  ) return "untitled";
+
+  const firstWords = str
+    .slice(str.indexOf("photo/") + 6)
+    .split("-")
+    .splice(0, maxWordCount)
+  
+  const lastWord = firstWords[firstWords.length - 1];
+  if (ignoredWordList.includes(lastWord)) {
+    return firstWords.splice(0, maxWordCount - 1).join(" ");
+  } else {
+    return firstWords.join(" ");
+  }
+}
+
+const ignoredWordList = [
+  "a",
+  "about",
+  "above",
+  "after",
+  "among",
+  "and",
+  "at",
+  "before",
+  "behind",
+  "below",
+  "beneath",
+  "beside",
+  "between",
+  "but",
+  "by",
+  "during",
+  "for",
+  "in",
+  "inside",
+  "into",
+  "like",
+  "near",
+  "of",
+  "off",
+  "on",
+  "onto",
+  "opposite",
+  "outside",
+  "over",
+  "past",
+  "than",
+  "through",
+  "till",
+  "to",
+  "toward",
+  "under",
+  "underneath",
+  "until",
+  "upon",
+  "versus",
+  "with",
+  "within",
+  "without",
+]
